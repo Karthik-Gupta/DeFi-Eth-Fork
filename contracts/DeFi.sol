29f7ca18944f000eee7f1b8451e7c1e5cf055f4d
Karthik-Gupta <karthikts.gupta@gmail.com>
2021-10-08 00:35:15 +0530

    Update DeFi

--- contracts/DeFi.sol
+++ contracts/DeFi.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
-import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
 
 contract DeFi {
     address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-    ISwapRouter private swapRouter;
+    ISwapRouter public immutable swapRouter;
 
-    function constuctor() public {
+    constructor() {
         swapRouter = ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
     }
     
@@ -28,9 +28,9 @@ contract DeFi {
             amountOutMinimum: 0,
             sqrtPriceLimitX96: 0
         });
-        
         TransferHelper.safeApprove(DAI, address(swapRouter), amountIn);
-        
+
+
         amountOut = swapRouter.exactInputSingle(params);
     }
 
